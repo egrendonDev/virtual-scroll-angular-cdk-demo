500c1dc75d7ec387dad2805bdd0ddb594ffa293a
Ernesto Rendon <egrendon.dev@gmail.com>
2020-04-08 10:51:03 -0700

    demo of POC

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { CdkVirtualScrollViewport } from '@angular/cdk/scrolling';
 
 @Component({
   selector: 'app-root',
@@ -7,10 +8,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   numbers: number[] = [];
+    @ViewChild(CdkVirtualScrollViewport) viewPort: CdkVirtualScrollViewport;
 
   constructor() {
-    for (let index = 0; index < 10000; index++) {
-      this.numbers.push(index);
-    }
+    this.resetList();
   }
+
+  public resetList() {
+      const randomInt = Math.floor(Math.random() * Math.floor(10000));
+
+      const newList = [];
+      for (let index = 0; index < randomInt; index++) {
+          newList.push(index);
+      }
+
+      // scroll to top
+      this.scrollToTop();
+      // make reassignment
+      this.numbers = newList;
+      console.log(randomInt);
+      console.log(this.numbers);
+      return this.numbers;
+  }
+
+    scrollToTop(): void {
+      if (this.viewPort) {
+          // this.viewPort.scrollToIndex(0, 'smooth');
+          this.viewPort.scrollToIndex(0);
+      }
+    }
+
+
 }
